682f6a7c71199194949a2827666081b3401c4741
Vikas Vinayak <vikasvinayakmaxim@gmail.com>
2013-12-06 08:36:31 -0800

    Cleaned up a few more unwanted lines

--- vinbutton.cpp
+++ vinbutton.cpp
@@ -1,6 +1,5 @@
 #include "vinbutton.h"
 #include "handleclick.h"
-#include <QMessageBox>
 
 vinButton::vinButton(QWidget *parent) :
     QPushButton(parent)
@@ -9,13 +8,9 @@ vinButton::vinButton(QWidget *parent) :
 
 void vinButton::mousePressEvent ( QMouseEvent * event )
 {
-    QString  s = "ff";
    handleClick *h = new handleClick;
    h->whichPress(this->text());
 
-    //emit xclicked(this->text());
-   QMessageBox *m = new QMessageBox;
-//    m->setText(this->text());
-//    m->setText(s);
-//    m->show();
+  //  emit xclicked(this->text());
+  //  not needed in this release
 }
